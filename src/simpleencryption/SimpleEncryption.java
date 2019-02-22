8aa3836f38368e0ad09b1692825ce60bd93d1b8a
kyluk1259 <47734907+kyluk1259@users.noreply.github.com>
2019-02-22 11:44:24 -0500

    Update SimpleEncryption.java

--- src/simpleencryption/SimpleEncryption.java
+++ src/simpleencryption/SimpleEncryption.java
@@ -32,29 +32,38 @@ public class SimpleEncryption {
         inOption = JOptionPane.showInputDialog("1 - Encryption\n2 - Decryption");
         choice = Integer.parseInt(inOption);
 
+        //set ending phrase to empty so that it can be filled with charactes 
         enPhrase = "";
 
+        //if user chooses to encrypt the input
                 if (choice == 1) {
             //compares character place to an array of the characters forming the input string
             for (char place : inPhrase.toCharArray()) {
+                //if char is a capital letter, set encrypted letter to uppercase character within range
                 if ((int) place >= 65 && (int) place <= 90) {
                     enPhrase = enPhrase + Character.toString((char) (((place - 'A' + rot) % 26) + 'A')).toUpperCase();
+                    //if char is not uppercase, set encrypted letter to lowercase character within range
                 } else {
                     enPhrase = enPhrase + Character.toString((char) (((place - 'a' + rot) % 26) + 'a'));
                 }
-            }
+            }       
+          //if user chooses to decrypt the input
         } else if (choice == 2) {
+            //compares character place to an array of the characters forming the input string
             for (char place : inPhrase.toCharArray()) {
-
+                //if char is a capital letter, set decrypted letter to uppercase character within range
                 if ((int) place >= 65 && (int) place <= 90) {
                     enPhrase = enPhrase + Character.toString((char) (((place - rot + 'A') % 26) + 'A')).toUpperCase();
+                  //if char is out of range, set decrypted letter to correct lowercase character within range
                 } else if ((int)place - rot < 97) {
                     enPhrase = enPhrase + Character.toString((char) ('z' - ('a' - (place - rot) - 1)));
+                  //if char is lowercase and in range, set decrypted letter
                 } else {
                     enPhrase = enPhrase + Character.toString((char) (place - rot));
                 }
             }
         }
+        //display ending phrase
         JOptionPane.showMessageDialog(null, enPhrase);
     }
 }
