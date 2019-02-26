a54aff287b82b5cc8018350a0d4b5221062c4cd2
kyluk1259 <47734907+kyluk1259@users.noreply.github.com>
2019-02-26 11:11:38 -0500

    Update SimpleEncryption.java

--- src/simpleencryption/SimpleEncryption.java
+++ src/simpleencryption/SimpleEncryption.java
@@ -22,7 +22,6 @@ public class SimpleEncryption {
         int rot, choice;
         //initialize phrase input window
         inPhrase = JOptionPane.showInputDialog("This program will encrypt or decrypt a phrase using the\nsimple encryption method of rotating the letters");
-        //newPhrase = "";
 
         //initialize rotation amount input window and parse to int
         inRot = JOptionPane.showInputDialog("Enter the rotation amount (1-25)");
@@ -35,31 +34,31 @@ public class SimpleEncryption {
         //set ending phrase to empty so that it can be filled with charactes 
         enPhrase = "";
 
-        //if user chooses to encrypt the input
+        //if user chooses to encrypt the input phrase
                 if (choice == 1) {
-            //compares character place to an array of the characters forming the input string
-            for (char place : inPhrase.toCharArray()) {
+            //compares character value to an array of the characters forming the input string
+            for (char value : inPhrase.toCharArray()) {
                 //if char is a capital letter, set encrypted letter to uppercase character within range
-                if ((int) place >= 65 && (int) place <= 90) {
-                    enPhrase = enPhrase + Character.toString((char) (((place - 'A' + rot) % 26) + 'A')).toUpperCase();
+                if ((int) value >= 65 && (int) value <= 90) {
+                    enPhrase = enPhrase + Character.toString((char) (((value - 'A' + rot) % 26) + 'A')).toUpperCase();
                     //if char is not uppercase, set encrypted letter to lowercase character within range
                 } else {
-                    enPhrase = enPhrase + Character.toString((char) (((place - 'a' + rot) % 26) + 'a'));
+                    enPhrase = enPhrase + Character.toString((char) (((value - 'a' + rot) % 26) + 'a'));
                 }
             }       
           //if user chooses to decrypt the input
         } else if (choice == 2) {
             //compares character place to an array of the characters forming the input string
-            for (char place : inPhrase.toCharArray()) {
+            for (char value : inPhrase.toCharArray()) {
                 //if char is a capital letter, set decrypted letter to uppercase character within range
-                if ((int) place >= 65 && (int) place <= 90) {
-                    enPhrase = enPhrase + Character.toString((char) (((place - rot + 'A') % 26) + 'A')).toUpperCase();
+                if ((int) value >= 65 && (int) value <= 90) {
+                    enPhrase = enPhrase + Character.toString((char) (((value - rot + 'A') % 26) + 'A')).toUpperCase();
                   //if char is out of range, set decrypted letter to correct lowercase character within range
-                } else if ((int)place - rot < 97) {
-                    enPhrase = enPhrase + Character.toString((char) ('z' - ('a' - (place - rot) - 1)));
+                } else if ((int)value - rot < 97) {
+                    enPhrase = enPhrase + Character.toString((char) ('z' - ('a' - (value - rot) - 1)));
                   //if char is lowercase and in range, set decrypted letter
                 } else {
-                    enPhrase = enPhrase + Character.toString((char) (place - rot));
+                    enPhrase = enPhrase + Character.toString((char) (value - rot));
                 }
             }
         }
